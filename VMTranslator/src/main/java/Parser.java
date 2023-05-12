b0fb57746ac11b2c5b5f1f7a984af48c69c56959
mishaalkandapath <mishaalhere@gmail.com>
2023-05-12 13:28:50 +0300

    parser skeleton

--- VMTranslator/src/main/java/Parser.java
+++ VMTranslator/src/main/java/Parser.java
@@ -1,5 +1,9 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Parser {
  /*
  Responsible for parsing a single.vm file
@@ -8,4 +12,22 @@ public class Parser {
  Push pop aka Memory Operations
  add, sub, or, neg, eq, or, and, lt, gt aka Arithmetic Operations
   */
+
+    private Scanner vmScannedFile;
+
+    /*
+    Constructor to accept a file and open it
+     */
+    public Parser(String filename, String extension) throws FileNotFoundException {
+        File vmFile = new File(filename + extension);
+        this.vmScannedFile = new Scanner(vmFile); //using a scanner for efficiency
+    }
+
+    public boolean hasMoreCommands(){
+        return vmScannedFile.hasNextLine();
+    }
+
+    public void advance(){
+        //
+    }
 }
